880d9a76f1a2fc8ecabdf3e1dd4600855ae7ff94
yeeway <alex20020609@gmail.com>
2023-03-27 15:35:57 +0800

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/pokedex-web-app/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
